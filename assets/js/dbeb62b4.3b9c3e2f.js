5023f50b0f8176b97daa144bc21955cc7c1577dc
Mansur Khairullin <whatdidu@gmail.com>
2025-07-03 15:40:50 +0300

    Updates

---
+++ assets/js/dbeb62b4.3b9c3e2f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmansur_docs=self.webpackChunkmansur_docs||[]).push([[3778],{7398:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/LobachevskyWeb/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
