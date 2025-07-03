e65780061a60ddfc4e0ea95bb70396c4db8cdd26
Mansur Khairullin <whatdidu@gmail.com>
2025-07-03 14:29:56 +0300

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,95 @@
+
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const config = {
+  title: 'Mansur Docs',
+  tagline: 'Документация по контроллерам и игрушкам',
+  url: 'https://whatdidu.github.io',
+  baseUrl: '/LobachevskyWeb/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  organizationName: 'Whatdidu', // GitHub org/user name.
+  projectName: 'LobachevskyWeb', // Repo name.
+
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/Whatdidu/LobachevskyWeb/edit/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/Whatdidu/LobachevskyWeb/edit/main/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Mansur Docs',
+        logo: {
+          alt: 'Mansur Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Документация',
+          },
+          {to: '/blog', label: 'Блог', position: 'left'},
+          {
+            href: 'https://github.com/Whatdidu/LobachevskyWeb',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Документация',
+            items: [
+              {
+                label: 'Начало',
+                to: '/docs',
+              },
+            ],
+          },
+          {
+            title: 'Сообщество',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Whatdidu/LobachevskyWeb',
+              },
+            ],
+          },
+        ],
+        copyright: `© ${new Date().getFullYear()} Mansur`,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ sidebars.js
@@ -0,0 +1,30 @@
+module.exports = {
+  tutorialSidebar: [
+    'index',
+    {
+      type: 'category',
+      label: 'Контроллеры',
+      items: ['controllers/esp32c3-mini'],
+    },
+    {
+      type: 'category',
+      label: 'Игрушки',
+      items: ['toys/placeholder'],
+    },
+    {
+      type: 'category',
+      label: 'Прошивка',
+      items: ['firmware/placeholder'],
+    },
+    {
+      type: 'category',
+      label: 'BLE Команды',
+      items: ['ble/placeholder'],
+    },
+    {
+      type: 'category',
+      label: 'FAQ',
+      items: ['faq/placeholder'],
+    },
+  ],
+};
